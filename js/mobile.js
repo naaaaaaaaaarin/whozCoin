4c179cca05939d87c09e31b20b2d71430fb47433
naaaaaaaaaarin <89621035+naaaaaaaaaarin@users.noreply.github.com>
2022-04-08 11:12:38 +0900

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,4 +1,13 @@
 $('.hamMenu').off('click').click(function(e){
   e.preventDefault();
   $('header nav').slideToggle(300);
+  if($(this).children('.ham').hasClass('open')){
+    $(this).children('.ham').removeClass('open');
+  }else {
+    $(this).children('.ham').addClass('open');
+  }
 });
+// $('.hamMenu').off('click').click(function(e){
+//   e.preventDefault();
+//   $('header nav').slideToggle(300);
+// });
